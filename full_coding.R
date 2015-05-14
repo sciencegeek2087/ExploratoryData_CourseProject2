abe9251fc3954fd9416a42e12ef8f9306c541a43
sciencegeek2087 <sciencegeek2087@users.noreply.github.com>
2015-05-14 12:10:05 +1200

    Update full_coding.R

--- full_coding.R
+++ full_coding.R
@@ -1,2 +1,4 @@
+unzip(exdata_data_NEI.zip, files = NULL, list = FALSE, overwrite = TRUE, junkpaths = FALSE, exdir = ".", unzip = "internal", setTimes = TRUE)
+
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
