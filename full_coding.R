04772353532446933008201cbf58300b1c166cf8
sciencegeek2087 <sciencegeek2087@users.noreply.github.com>
2015-05-14 12:14:39 +1200

    Update full_coding.R

--- full_coding.R
+++ full_coding.R
@@ -1,4 +1,8 @@
+#note: exdata_data_NEI.zip needs to be in local directory.
 unzip(exdata_data_NEI.zip, files = NULL, list = FALSE, overwrite = TRUE, junkpaths = FALSE, exdir = ".", unzip = "internal", setTimes = TRUE)
 
+# read the emmissions data file for 1999, 2002, 2005, & 2008
 NEI <- readRDS("summarySCC_PM25.rds")
+
+# read the Source Classification Code Table - gives us a name of factor to use instead of binary code.
 SCC <- readRDS("Source_Classification_Code.rds")
